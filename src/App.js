ad2ca4f332a4e67ea8764f087dbbbf3daba44ee9
alitaee <ali.taee.72@gmail.com>
2020-05-27 00:07:45 +0430

    Simple component for learning test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [click, setClick] = useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{click}</p>
+        <button className="App-button" onClick={() => setClick(click + 1)}>
+          Plus
+        </button>
+        <button className="App-button" onClick={() => setClick(click - 1)}>
+          Minus
+        </button>
       </header>
     </div>
   );
