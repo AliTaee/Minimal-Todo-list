f5759fee25af2934cb86274430c9d0f3d2cc19a8
alitaee <ali.taee.72@gmail.com>
2020-05-27 23:46:26 +0430

    Add 3 test for our minimal todo list

--- src/App.test.js
+++ src/App.test.js
@@ -2,44 +2,31 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-describe("Test Plus and Minus state", () => {
+describe("Test Todo list", () => {
+  const todoList = [
+    { id: 1, title: "Buy milk", done: false },
+    { id: 2, title: "Learning React", done: false },
+  ];
+
   beforeEach(() => {
-    render(<App num={10} />);
+    render(<App />);
   });
 
-  test("Initial number must be 10", () => {
-    expect(screen.getByText(/10/i)).toBeInTheDocument();
+  test("Must have 2 note on list", () => {
+    expect(screen.getByText(todoList[0].title)).toBeInTheDocument();
+    expect(screen.getByText(todoList[1].title)).toBeInTheDocument();
   });
 
-  test("Renders Plus button", () => {
-    const plusButton = screen.getByText(/plus/i);
-    expect(plusButton).toBeInTheDocument();
+  test("There must be Button to add note to list", () => {
+    const addButton = screen.getByText(/Add to list/i);
+    expect(addButton).toBeInTheDocument();
   });
 
-  test("Click on Plus button and it's should show 11", () => {
-    const plusButton = screen.getByText(/plus/i);
-    fireEvent.click(plusButton);
-
-    expect(screen.getByText(/11/i)).toBeInTheDocument();
-  });
-
-  test("Click two time on Plus button and it's should show 12", () => {
-    const plusButton = screen.getByText(/plus/i);
-    fireEvent.click(plusButton);
-    fireEvent.click(plusButton);
-
-    expect(screen.getByText(/12/i)).toBeInTheDocument();
-  });
-
-  test("Renders Minus button", () => {
-    const MinusButton = screen.getByText(/minus/i);
-    expect(MinusButton).toBeInTheDocument();
-  });
-
-  test("Click on Minus button and it's should show 9", () => {
-    const MinusButton = screen.getByText(/minus/i);
-    fireEvent.click(MinusButton);
-
-    expect(screen.getByText(/9/i)).toBeInTheDocument();
+  test("Add new note", () => {
+    fireEvent.change(screen.getByLabelText(/Add New note/i), {
+      target: { value: "learning unit testing" },
+    });
+    fireEvent.click(screen.getByText(/Add to list/i));
+    expect(screen.getByText("learning unit testing")).toBeInTheDocument();
   });
 });
