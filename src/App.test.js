7d55e639a4c4dabddbc4f7c4330a80a01dc99c3c
alitaee <ali.taee.72@gmail.com>
2020-05-27 22:54:25 +0430

    Testing component with props

--- src/App.test.js
+++ src/App.test.js
@@ -4,11 +4,11 @@ import App from "./App";
 
 describe("Test Plus and Minus state", () => {
   beforeEach(() => {
-    render(<App />);
+    render(<App num={10} />);
   });
 
-  test("Initial number must be 0", () => {
-    expect(screen.getByText(/0/i)).toBeInTheDocument();
+  test("Initial number must be 10", () => {
+    expect(screen.getByText(/10/i)).toBeInTheDocument();
   });
 
   test("Renders Plus button", () => {
@@ -16,19 +16,19 @@ describe("Test Plus and Minus state", () => {
     expect(plusButton).toBeInTheDocument();
   });
 
-  test("Click on Plus button and it's should show 1", () => {
+  test("Click on Plus button and it's should show 11", () => {
     const plusButton = screen.getByText(/plus/i);
     fireEvent.click(plusButton);
 
-    expect(screen.getByText(/1/i)).toBeInTheDocument();
+    expect(screen.getByText(/11/i)).toBeInTheDocument();
   });
 
-  test("Click two time on Plus button and it's should show 2", () => {
+  test("Click two time on Plus button and it's should show 12", () => {
     const plusButton = screen.getByText(/plus/i);
     fireEvent.click(plusButton);
     fireEvent.click(plusButton);
 
-    expect(screen.getByText(/2/i)).toBeInTheDocument();
+    expect(screen.getByText(/12/i)).toBeInTheDocument();
   });
 
   test("Renders Minus button", () => {
@@ -36,11 +36,10 @@ describe("Test Plus and Minus state", () => {
     expect(MinusButton).toBeInTheDocument();
   });
 
-  test("Click on Minus button and it's should show -1", () => {
+  test("Click on Minus button and it's should show 9", () => {
     const MinusButton = screen.getByText(/minus/i);
     fireEvent.click(MinusButton);
 
-    expect(screen.getByText(/-1/i)).toBeInTheDocument();
-    screen.debug();
+    expect(screen.getByText(/9/i)).toBeInTheDocument();
   });
 });
