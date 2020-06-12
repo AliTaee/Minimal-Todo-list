47fcdb527fa8792aef165c22cf08f7e3cc21b5a7
alitaee <ali.taee.72@gmail.com>
2020-06-13 00:24:54 +0430

    Improve empty error message and note item UI and UX

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ function App(props) {
   const { todo } = props;
   const [todoList, setTodoList] = useState(todo);
   const [toAddList, setToAddList] = useState("");
+  const [error, setError] = useState({ state: false, message: "" });
 
   const handelAddToDo = () => {
-    let maxId = 0;
-    todoList.forEach((todo) => {
-      if (todo.id > maxId) maxId = todo.id;
-    });
-    maxId++;
-    // Add a new todo list
-    setTodoList([...todoList, { id: maxId, title: toAddList, done: false }]);
-    // Clear input field
-    setToAddList("");
+    if (toAddList === "") {
+      setError({ state: true, message: "You can't add empty note!" });
+      return;
+    } else {
+      let maxId = 0;
+      todoList.forEach((todo) => {
+        if (todo.id > maxId) maxId = todo.id;
+      });
+      maxId++;
+      // Add a new todo list
+      setTodoList([...todoList, { id: maxId, title: toAddList, done: false }]);
+      // Clear input field
+      setToAddList("");
+      // Clear Pre error message
+      setError({ state: false, message: "" });
+    }
   };
 
   // Toggle todo state
@@ -41,16 +49,21 @@ function App(props) {
   return (
     <div className="App">
       <header className="App-header">
+        {error.state && <p className="error">{error.message}</p>}
         {todoList.length ? (
           <ul className="todo-list">
-            {todoList.map((todo) => (
-              <li key={todo.id}>
-                {todo.title}
+            {todoList.map((todo, index) => (
+              <li
+                className={todo.done ? "note-item note-done" : "note-item"}
+                key={todo.id}
+              >
                 <input
+                  id={`note-state-${index}`}
                   type="checkbox"
                   value={todo.done}
                   onChange={() => toggleTodoState(todo.id)}
                 />
+                <label htmlFor={`note-state-${index}`}>{todo.title}</label>
                 <button
                   onClick={() => deleteTodo(todo.id)}
                   className="todo-delete"
